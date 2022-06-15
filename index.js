af94d6ffb5e5d895008a962839d641a9fb34001f
Heng Yu <heng.yu161@gmail.com>
2022-06-16 02:21:05 +0800

    Finish all 'view all **' options in menu.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const Menu = require('../UCSD_HW10_Team-Profile-Generator/src/Menu');
 
 const PORT = 3001;
 const app = express();
@@ -18,20 +19,22 @@ const db = mysql.createConnection(
     console.log("Connected to the employee_db database.")
 );
 
-inquirer
+const MenuChoose = () => {
+    inquirer
   .prompt([
     {
       type: "list",
       name: "motivation",
       message: "What would you like to do?",
       choices: [
-                "Add Employee", 
-                "Update Employee Role", 
-                "View All Roles", 
-                "Add Role", 
-                "View All Departments", 
-                "Add Department",
-                "quit"
+          "View All Employee",
+          "Add Employee", 
+          "Update Employee Role", 
+          "View All Roles", 
+          "Add Role", 
+          "View All Departments", 
+          "Add Department",
+          "Quit"
                 ],
     }
   ])
@@ -45,7 +48,7 @@ inquirer
             updateEmployeeRole();
             break;
         case "View All Roles":
-            ViewAllRoles();
+            viewAllRoles();
             break;
         case "Add Role":
             addRole();
@@ -56,13 +59,69 @@ inquirer
         case "Add Department":
             addDepartment();
             break;
-        case "quit":
+        case "View All Employee":
+            viewAllEmployee();
+            break;
+        case "Quit":
             db.end();
             break;
     }
-  })
+  });
+};
+
+const viewAllDepartments = () => {
+    const sql = "SELECT id, name FROM department";
+    db.query(sql, (err, results) => {
+        if(err) {
+            res.status(500).json({ error: err.message});
+            return;
+        }
+        displayTable(results);
+    })
+    MenuChoose();
+};
+
+const viewAllRoles = () => {
+    const sql = `SELECT role.id, 
+                role.title, 
+                department.name AS department, 
+                role.salary 
+                FROM role JOIN department 
+                ON department.id = role.department_id;`
+    db.query(sql, (err, results) => {
+        if(err) {
+            res.status(500).json({ error: err.message});
+            return;
+        }
+        displayTable(results);
+    })
+    MenuChoose();
+};
+
+const viewAllEmployee = () => {
+    const sql = `SELECT sup.id, sup.first_name, sup.last_name, role.title AS title, department.name AS department, role.salary, CONCAT(sub.first_name, ' ', sub.last_name) AS manager
+    FROM employee sup
+    LEFT JOIN role ON role.id = sup.id
+    LEFT JOIN department ON department.id = role.department_id
+    LEFT JOIN employee sub ON sub.id = sup.manager_id;`
+    db.query(sql, (err,results) => {
+        if(err) {
+            res.status(500).json({ error: err.message});
+            return;
+        }
+        displayTable(results);
+    })
+    MenuChoose();
+}
+
+const displayTable = (table) => {
+    console.log("\n");
+    console.table(table);
+    console.log("\n");
+};
 
 
+MenuChoose();
 
 // app.listen(PORT, () => {
 //     console.log(`Server running on port ${PORT}`);
